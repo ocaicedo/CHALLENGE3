4367c597877e1a5da4b172894b24cb1238b69bda
Oscar <caicedo.os@gmail.com>
2015-02-23 17:47:31 -0500

    se corrige método de validacion de atributos de Player: email y active

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ActiveRecord::Base
-	validates :email, presence: true, format: /@/
+	validates :email, presence: true, format: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
 	validates :name, presence: true
 	validates :username, length: {in: 5..20}, uniqueness: true
 	validates :points, numericality: {greater_than_or_equal_to: 0}
+	validates :active, inclusion: {in: [true, false]}
 end
